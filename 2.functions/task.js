1ed9ecd154f1c76a28da94afc64b0d1601f24030
Вячеслав Хохлов <slava071172@mail.ru>
2022-11-23 12:32:11 +0300

    kommit

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,8 +1,25 @@
-// Задание 1
-function getArrayParams(arr) {
-  let min, max, sum, avg;
+"use strict";
 
+// Задание 1
+
+function getArrayParams(arr) {
   // Ваш код
+  let min = Infinity; 
+  let max = -Infinity; 
+  let sum = 0; 
+  let avg = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    sum = sum + arr[i];
+    if (arr[i] > max) {
+      max = arr[i];
+    }
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+  } 
+
+avg = parseFloat((sum / arr.length).toFixed(2));
 
   return { min: min, max: max, avg: avg };
 }
