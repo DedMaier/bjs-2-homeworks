45cee3eaea7d935aeef4396e250524db3bdf8eb9
Вячеслав Хохлов <slava071172@mail.ru>
2022-11-23 11:27:56 +0300

    kommit

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,6 +1,20 @@
+"use strict";
+
 function solveEquation(a, b, c) {
-  let arr;
+  let arr = [];
   // код для задачи №1 писать здесь
+  let d = b ** 2 - 4 * a * c;
+
+  if (d < 0) {
+  } else if (d === 0) {
+    let x = -b / (2 * a);
+    arr.push(x);
+  } else if (d > 0) {
+    let x1 = (-b + Math.sqrt(d)) / (2 * a);
+    let x2 = (-b - Math.sqrt(d)) / (2 * a);
+    arr.push(x1, x2);
+  }
+  console.log(arr);
   return arr; // array
 }
 
