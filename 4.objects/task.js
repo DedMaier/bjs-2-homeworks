bdae3a9f72bde23d83e25b8eb46289b5ba880403
Вячеслав Хохлов <slava071172@mail.ru>
2023-03-23 20:35:34 +0300

    kommit. задача 1.

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,39 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+  // Ваш код
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  this.marks = [];
 }
 
 Student.prototype.setSubject = function (subjectName) {
   //ваш код
-}
+  this.subject = subjectName;
+};
 
 // ваш код для остальных методов
+Student.prototype.addMarks = function (...marks) {
+  if (this.hasOwnProperty(`marks`) === true) {
+    this.marks.push(...marks);
+  }
+};
+
+Student.prototype.getAverage = function () {
+  if (this.hasOwnProperty(`marks`) === false || this.marks.length === 0) {
+    return 0;
+  }
+  const gradePointAverage = this.marks.reduce((acc, mark, index, arr) => {
+    acc += mark;
+    if (index === arr.length - 1) {
+      return acc / arr.length;
+    }
+    return acc;
+  }, 0);
+  return gradePointAverage;
+};
+
+Student.prototype.exclude = function (reason) {
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
+};
